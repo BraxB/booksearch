08a0cafefd7ed20ba9fd44f7bc5bee59a3209f8b
BraxB <braxb92@gmail.com>
2021-07-15 22:42:18 -0400

    added mutations and queries. Getting server error. Tried to troubleshoot. No dice.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,7 +11,7 @@ const typeDefs = gql`
 
     type Auth {
         token: ID!
-        profile: Profile
+        user: User
     }
 
     type Book {
--- server/server.js
+++ server/server.js
@@ -1,36 +1,38 @@
 const express = require('express');
-// added new const to require the apollo server package
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// created const for typeDefs and Resolvers to set schema and equivalent of the routes currently being used for Apollo
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-// const routes = require('./routes');
 
-const app = express();
+const { typeDefs, resolvers } = require('./schemas');
+// Import `authMiddleware()` function to be configured with the Apollo Server
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
 });
 
-server.applyMiddleware({app})
+server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// for REST version... commented out as we are using Apollo
-// app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
-  console.log(`🌍 Now listening on localhost:${PORT}`);
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
